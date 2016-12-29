5faf34c7d9f688e30348bba5c0f19154e00fa52e
hengshuai yao <hengshuai@gmail.com>
2016-12-28 23:31:57 -0800

    working but not perfect

--- obstacle_avoidance_car_shield.ino
+++ obstacle_avoidance_car_shield.ino
@@ -1,26 +1,54 @@
+#include <Servo.h>
+
 const int L298N_IN1 = 11;
 const int L298N_IN2 = 10;
 const int L298N_IN3 = 9;
 const int L298N_IN4 = 8;
 
-//const int trigPin = 13;
-//const int echoPin = 12;
-//float travelTime = 0;
-//const float speedOfSound = 776.5;//miles per hour
-//float distance = 0;
+Servo sr04_servo;
+const int SERVO_PIN = 6;
+
+
+const int TRIG_PIN = 13;
+const int ECHO_PIN = 12;
+float travelTime = 0;
+const float SPEED_OF_SOUND = 776.5;//miles per hour
+
+const int DEGREE_AHEAD = 85;
+const int DEGREE_LEFT = 120;
+const int DEGREE_RIGHT = 50;
   
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600); 
-//  pinMode(trigPin, OUTPUT);
-//  pinMode(echoPin, INPUT);
+    Serial.begin(9600); 
+    pinMode(TRIG_PIN, OUTPUT);
+    pinMode(ECHO_PIN, INPUT);
+  
+    pinMode(L298N_IN1, OUTPUT);
+    pinMode(L298N_IN2, OUTPUT);
+    pinMode(L298N_IN3, OUTPUT);
+    pinMode(L298N_IN4, OUTPUT);
+  
+    sr04_servo.attach(SERVO_PIN);
+    sr04_servo.write(DEGREE_AHEAD);
 
-  pinMode(L298N_IN1, OUTPUT);
-  pinMode(L298N_IN2, OUTPUT);
-  pinMode(L298N_IN3, OUTPUT);
-  pinMode(L298N_IN4, OUTPUT);
+    delay(2000);
+    distance();
+    delay(100);
+    distance();
+    delay(100);
+    distance();
+    delay(100);
+    distance();
+    delay(100);
 }
 
+void stop(){
+    digitalWrite(L298N_IN1, LOW);
+    digitalWrite(L298N_IN2, LOW);
+    digitalWrite(L298N_IN3, LOW);
+    digitalWrite(L298N_IN4, LOW);
+}
 
 void forward(){
     digitalWrite(L298N_IN1, HIGH);
@@ -41,6 +69,8 @@ void turn_right(){
     digitalWrite(L298N_IN2, LOW);
     digitalWrite(L298N_IN3, HIGH);
     digitalWrite(L298N_IN4, LOW);
+    delay(300);
+    forward();
 }
 
 void turn_left(){
@@ -48,29 +78,78 @@ void turn_left(){
     digitalWrite(L298N_IN2, HIGH);
     digitalWrite(L298N_IN3, LOW);
     digitalWrite(L298N_IN4, HIGH);
+    delay(300);
+    forward();
+}
+
+int right_space(){
+    sr04_servo.write(DEGREE_RIGHT);
+//    delay(500);
+    float dist = distance();
+//    sr04_servo.write(DEGREE_AHEAD);
+//    delay(100);
+    return dist;
+}
+
+int left_space(){
+    sr04_servo.write(DEGREE_LEFT);
+//    delay(500);
+    float dist = distance();
+//    sr04_servo.write(DEGREE_AHEAD);
+//    delay(100);
+    return dist;
 }
 
 
+int distance(){
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2000);
+
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(15);
+
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(10);
+
+  travelTime = pulseIn(ECHO_PIN, HIGH);
+  travelTime /= 1000000;
+  travelTime /= 3600;
+
+  float distance = SPEED_OF_SOUND * travelTime / 2.0;
+  distance *= (1.60934 * 1000 * 100); 
+  return distance;
+}
+
 
 void loop() {
-  backward();
-  
- 
-//  digitalWrite(trigPin, LOW);
-//  delayMicroseconds(2000);
-//
-//  digitalWrite(trigPin, HIGH);
-//  delayMicroseconds(15);
-//
-//  digitalWrite(trigPin, LOW);
-//  delayMicroseconds(10);
-//
-//  travelTime = pulseIn(echoPin, HIGH);
-//  travelTime /= 1000000;
-//  travelTime /= 3600;
-//
-//  distance = speedOfSound * travelTime / 2.0;
-//  distance *= (1.60934 * 1000 * 100); 
-//  Serial.println(distance);
-
+  sr04_servo.write(DEGREE_AHEAD);
+  delay(100);
+  float front_dist = distance();
+  Serial.print("front_dist=");
+  Serial.println(front_dist);
+  if(front_dist < 20){
+      stop();
+      delay(100);
+      backward();
+      delay(100);
+      stop();
+      delay(100);
+      float left_dist = left_space();
+      delay(500);
+      float right_dist = right_space();
+      Serial.print("left_dist = ");
+      Serial.println(left_dist);
+      Serial.print("right_dist = ");
+      Serial.println(right_dist);
+      if(left_dist > right_dist){
+          turn_left();  
+          Serial.println("turn left!");
+      }else{
+          turn_right();  
+          Serial.println("turn right!");
+      }
+      stop();
+  }else{
+    forward();  
+  }
 }
